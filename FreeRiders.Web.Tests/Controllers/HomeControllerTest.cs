1e77881d0c82691aa8659380e81ef4a9f8da732e
yasenm <ias_sv_bg@yahoo.co.uk>
2014-11-10 19:38:55 +0200

    Tests issues fixed

---
+++ FreeRiders.Web.Tests/Controllers/HomeControllerTest.cs
@@ -0,0 +1,16 @@
+﻿namespace FreeRiders.Web.Tests.Controllers
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Web.Mvc;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using FreeRiders.Web;
+    using FreeRiders.Web.Controllers;
+
+    [TestClass]
+    public class HomeControllerTest
+    {
+    }
+}
